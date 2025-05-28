09b75ee50f4aba0368233bdaef2d66d71faf2800
Fahril Hadi <fahrilhadi77@gmail.com>
2025-05-28 23:05:49 +0700

    database connection

---
+++ gorm_test.go
@@ -0,0 +1,25 @@
+package golang_gorm
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func OpenConnection() *gorm.DB {
+	dialect := mysql.Open("root:@tcp(localhost:3306)/db_golang-gorm?charset=utf8mb4&parseTime=True&loc=Local")
+	db, err := gorm.Open(dialect, &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	return db
+}
+
+var db = OpenConnection()
+
+func TestOpenConnection(t *testing.T)  {
+	assert.NotNil(t, db)
+}

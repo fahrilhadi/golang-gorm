1a30c989ef58018700ce691b4429d98f7d57c1d1
Fahril Hadi <fahrilhadi77@gmail.com>
2025-06-01 20:52:25 +0700

    field permission

--- user.go
+++ user.go
@@ -3,11 +3,12 @@ package golang_gorm
 import "time"
 
 type User struct {
-	ID string `gorm:"primary_key;column:id"`
+	ID string `gorm:"primary_key;column:id;<-:create"`
 	Password string `gorm:"column:password"`
 	Name string `gorm:"column:name"`
-	CreatedAt time.Time `gorm:"column:created_at;autoCreateTime"`
+	CreatedAt time.Time `gorm:"column:created_at;autoCreateTime;<-:create"`
 	UpdatedAt time.Time `gorm:"column:updated_at;autoCreateTime;autoUpdateTime"`
+	Information string `gorm:"-"`
 }
 
 func (u *User) TableName() string {
